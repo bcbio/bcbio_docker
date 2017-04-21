b505922548039363672d9a8be7b138dea8c064fd
chapmanb <chapmanb@50mail.com>
2017-04-21 05:24:25 -0400

    Fix multi-tag builds and update README links

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -ex -o pipefail
 BCBIO_VERSION="1.0.3a"
 BCBIO_REVISION="c11d222"
 NS="quay.io/bcbio"
-VERSION_TAG="${BCBIO_VERSION}-${BCBIO_REVISION}"
+TAG="${BCBIO_VERSION}-${BCBIO_REVISION}"
 
 # build bcbio base
 docker pull ubuntu:16.04
-docker build --no-cache --build-arg "git_revision=${BCBIO_REVISION}" -t "${NS}/bcbio-base:${BASE_TAG}" -t "${NS}/bcbio-base:latest" - < Dockerfile.base
+docker build --no-cache --build-arg "git_revision=${BCBIO_REVISION}" -t "${NS}/bcbio-base:${TAG}" -t "${NS}/bcbio-base:latest" - < Dockerfile.base
 # build bcbio + task specific tools
 
 # log in to quay.io
@@ -18,4 +18,4 @@ docker login -u="bcbio+travis" -p="$QUAY_PASSWORD" quay.io
 # push images
 set -ex -o pipefail
 docker push "${NS}/bcbio-base:latest"
-docker push "${NS}/bcbio-base:${BASE_TAG}"
+docker push "${NS}/bcbio-base:${TAG}"
