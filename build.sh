8275f32063e535436f377e84087b984c423635b8
chapmanb <chapmanb@50mail.com>
2017-04-23 14:32:44 -0400

    Correctly reference external tool YAML in context
    Also imports latest bcbio which works around Travis-CI log limits

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -ex -o pipefail
 
 BCBIO_VERSION="1.0.3a"
-BCBIO_REVISION="a5aa1aa"
+BCBIO_REVISION="7f4fb4c"
 TOOLS="bcbio-align"
 NS="quay.io/bcbio"
 TAG="${BCBIO_VERSION}-${BCBIO_REVISION}"
@@ -14,7 +14,7 @@ docker build --no-cache --build-arg "git_revision=${BCBIO_REVISION}" -t "${NS}/b
 # build bcbio + task specific tools
 for TOOL in ${TOOLS}
 do
-    docker build --no-cache --build-arg "git_revision=${BCBIO_REVISION}" --build-arg "tool_yaml=packages/${TOOL}.yaml" -t "${NS}/${TOOL}:${TAG}" -t "${NS}/${TOOL}:latest" -f Dockerfile.tools .
+    docker build --no-cache --build-arg "git_revision=${BCBIO_REVISION}" --build-arg "tool=${TOOL}" -t "${NS}/${TOOL}:${TAG}" -t "${NS}/${TOOL}:latest" -f Dockerfile.tools .
 done
 
 # log in to quay.io
