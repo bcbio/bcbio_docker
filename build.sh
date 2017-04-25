a5b57b20b4996f5f991d397b67ce3f7e47fe52ef
chapmanb <chapmanb@50mail.com>
2017-04-25 12:06:49 -0400

    Revert back to single build script
    Parallel docker push did not help with timeouts, so limit to new
    bcbio-vc and bcbio-qc containers. Need to work on image size to get
    faster builds or have a matrix of builds.

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex -o pipefail
 
+BCBIO_VERSION="1.0.3a"
+BCBIO_REVISION="3be5db2"
+NS="quay.io/bcbio"
 TAG="${BCBIO_VERSION}-${BCBIO_REVISION}"
 
 # build bcbio base
@@ -11,4 +14,18 @@ docker build --no-cache --build-arg "git_revision=${BCBIO_REVISION}" -t "${NS}/b
 for TOOL in ${TOOLS}
 do
     docker build --no-cache --build-arg "git_revision=${BCBIO_REVISION}" --build-arg "tool=${TOOL}" -t "${NS}/${TOOL}:${TAG}" -t "${NS}/${TOOL}:latest" -f Dockerfile.tools .
+done
+
+# log in to quay.io
+set +x # avoid leaking encrypted password into travis log
+docker login -u="bcbio+travis" -p="$QUAY_PASSWORD" quay.io
+
+# push images
+set -ex -o pipefail
+docker push "${NS}/bcbio-base:${TAG}"
+docker push "${NS}/bcbio-base:latest"
+for TOOL in ${TOOLS}
+do
+    docker push "${NS}/${TOOL}:${TAG}"
+    docker push "${NS}/${TOOL}:latest"
 done
