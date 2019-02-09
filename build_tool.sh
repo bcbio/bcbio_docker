27ad0e92bd3fbe48da40253118521cba16e88a87
chapmanb <chapmanb@50mail.com>
2019-02-09 05:19:44 -0500

    Python 3 compatibility; add gsort to container

--- build_tool.sh
+++ build_tool.sh
@@ -6,6 +6,7 @@ TAG="${BCBIO_VERSION}-${BCBIO_REVISION}"
 df -h
 docker build --no-cache --build-arg "git_revision=${TAG}" --build-arg "tool=${TOOL}" -t "${NS}/${TOOL}:${TAG}" -t "${NS}/${TOOL}:latest" -f Dockerfile.tools .
 df -h
+docker images
 
 # log in to quay.io
 set +x # avoid leaking encrypted password into travis log
